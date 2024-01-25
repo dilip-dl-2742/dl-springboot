a0a8876880a82b6680d615f599a2a931b3024b2d
Dilip Kumar Doddi <ddoddi@msa.miraclesoft.com>
2024-01-25 03:48:07 -0500

    Uploading the code

---
+++ src/main/java/test/test.java
@@ -0,0 +1,13 @@
+package test;
+
+@RestController
+public class test {
+	public int main() {
+	System.out.println("First Message printed on Springboot with Status code: 200");
+	
+	int a = 10;
+	int b = 20;
+	return a+b;
+	
+	}
+}
